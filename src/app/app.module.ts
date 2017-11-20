5906b2f389bd99721a09832c241a416d2687e790
Kevin Broeren <info@kevinbroeren.nl>
2017-11-20 15:44:15 +0100

    Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Http, HttpModule } from "@angular/http";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        HttpModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Message } from "../../message/message";
+import { Http } from "@angular/http";
+import { Headers } from '@angular/http';
 
 @Component({
     selector: 'page-home',
@@ -9,8 +11,11 @@ import { Message } from "../../message/message";
 export class HomePage {
 
     public messages: [Message];
+    private api_url = "https://api.dialogflow.com/v1/";
+    private client_access_token = "fc780a693fc74b1c847a99bdbdcbb850";
+    private developer_access_token = "0b465c64ade544deb24ccfc63a994f0d";
 
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private http: Http) {
         this.messages = [
             new Message("Hello there!", true),
             new Message("Hi Kevin", false),
@@ -22,8 +27,36 @@ export class HomePage {
     public onKeyUp(pressedKey) {
         const returnKeyKeyCode = 13;
         if (pressedKey.keyCode == returnKeyKeyCode) {
-            // stuff to do
+            console.log("Return key pressed.");
+            this.doRequest();
+        } else {
+            console.log("Another key pressed.", pressedKey.keyCode);
         }
+    }
+
+    private doRequest() {
+        console.log("Executing request.");
+
+        const httpOptions = {
+            headers: new Headers({ 'Authorization': 'Bearer' + this.client_access_token })
+        };
+
+        this.http.get('https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=en&query=apple&sessionId=12345&timezone=America/New_York', httpOptions)
+            .subscribe(data => {
+
+                console.log(data.status);
+                // console.log(data.data); // data received by server
+                console.log(data.headers);
+
+            })
+            // .catch(error => {
+            //
+            //     console.log(error.status);
+            //     console.log(error.error); // error message as string
+            //     console.log(error.headers);
+            //
+            // });
+
     }
 
 }
