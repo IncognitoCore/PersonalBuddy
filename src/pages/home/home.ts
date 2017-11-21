2ea88dcf455fbed8fe459cde913a1f6fcf4f04a7
Kevin Broeren <info@kevinbroeren.nl>
2017-11-21 13:14:21 +0100

    App working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,12 +15,11 @@ export class HomePage {
     private client_access_token = "fc780a693fc74b1c847a99bdbdcbb850";
     private developer_access_token = "0b465c64ade544deb24ccfc63a994f0d";
 
+    public query: string = "";
+
     constructor(public navCtrl: NavController, private http: Http) {
         this.messages = [
-            new Message("Hello there!", true),
-            new Message("Hi Kevin", false),
-            new Message("How are you?", true),
-            new Message("I am good, fine?", false)
+            new Message("Hey Kevin, stel mij al je vragen over de Efteling!", false),
         ];
     }
 
@@ -29,26 +28,48 @@ export class HomePage {
         if (pressedKey.keyCode == returnKeyKeyCode) {
             console.log("Return key pressed.");
             this.doRequest();
-        } else {
-            console.log("Another key pressed.", pressedKey.keyCode);
         }
     }
 
     private doRequest() {
-        console.log("Executing request.");
+        const currentQuery = this.query;
+        this.query = "";
+        console.log("Executing request for query:", currentQuery);
+        this.messages.push(new Message(currentQuery, true));
 
         const httpOptions = {
             headers: new Headers({ 'Authorization': 'Bearer' + this.client_access_token })
         };
 
-        this.http.get('https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=en&query=apple&sessionId=12345&timezone=America/New_York', httpOptions)
+        //https://api.dialogflow.com/v1/query?v=20150910&contexts=shop&lang=en&query=apple&sessionId=12345&timezone=America/New_York
+
+        const body = {
+            "contexts": [
+            ],
+            "lang": "nl",
+            "query": currentQuery,
+            "sessionId": "12345",
+            "timezone": "America/New_York"
+        };
+
+        this.http.post('https://api.dialogflow.com/v1/query?v=20150910', body, httpOptions)
             .subscribe(data => {
 
-                console.log(data.status);
-                // console.log(data.data); // data received by server
-                console.log(data.headers);
+                if(data.status == 200) {
+                    const result = data.json().result;
+                    console.log(result);
+                    if (result.action != 'input.unknown') {
+                        this.messages.push(new Message(result.fulfillment.speech, false))
+                    } else {
+                        console.log("Ik begrijp je niet.");
+                        this.messages.push(new Message("Ik begrijp je niet.", true));
+                        console.log(result);
+                    }
+                } else {
+                    console.log("Error, status code:", data.status);
+                }
 
-            })
+            });
             // .catch(error => {
             //
             //     console.log(error.status);
